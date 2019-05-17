be2fc58aee455c2170c18129093672c5168df369
Mickaël Andrieu <mickael.andrieu@prestashop.com>
2019-05-17 16:30:18 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+## Grid collector for PrestaShop
+
+### Requirements
+
+ 1. PrestaShop 1.7.5+
+ 2. PHP 7.1 at least.
+ 
+### How to install
+
+ 1. Download or clone module into `modules` directory of your PrestaShop installation
+ 2. Make sure module directory is named `gridcollector`
+ 3. `cd` into module's directory and run following command:
+	 - `composer dumpautoload` to generate autoloader for module
+ 4. Install module from Back Office
---
+++ gridcollector.php
@@ -0,0 +1,23 @@
+<?php
+
+require_once __DIR__.'/vendor/autoload.php';
+
+class gridcollector extends Module
+{
+    /**
+     * In constructor we define our module's meta data.
+     * It's better tot keep constructor (and main module class itself) as thin as possible
+     * and do any processing in controller.
+     */
+    public function __construct()
+    {
+        $this->name = 'gridcollector';
+        $this->version = '1.0.0';
+        $this->author = 'Mickaël Andrieu';
+        $this->need_instance = 0;
+
+        parent::__construct();
+
+        $this->displayName = 'GridCollector module';
+    }
+}
